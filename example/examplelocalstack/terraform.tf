98b3f8d3fefd6221cf5151ca08f833d7cb499558
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:59:24 +0100

    fix: add external provider source, loosen constraints, upgrade lock file

--- example/examplelocalstack/terraform.tf
+++ example/examplelocalstack/terraform.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      version = "4.6.0"
       source  = "hashicorp/aws"
+      version = ">= 4.6.0"
     }
     external = {
-      version = "2.0.0"
+      source  = "hashicorp/external"
+      version = ">= 2.0.0"
     }
   }
 }
